9d6b469d6eb7d1f156bea7846dce297a721fd702
cherylritaomondi@gmail.com <ritaOmondi>
2023-08-30 00:42:34 -0700

    exponential value: betty coding style

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
  * _pow_recursion - callculating x to the power of y
- * @x - value to multiply
- * @y - the times the number is being multiplied
- * Return - integer value
+ * @x : the base value
+ * @y : the exponent value
+ * Return : he result of x raised to the power of y.
  */
 int _pow_recursion(int x, int y)
 {
