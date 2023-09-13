37a3016cee34224f347ec01c6bb54676eccaa2c1
cherylritaomondi@gmail.com <ritaOmondi>
2023-09-13 00:20:55 -0700

    performing simple operations

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,33 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * get_op_func - Funbctions selects the correct operation,
+ * to perform as required by user
+ * @s : operator passed as argument to the program
+ *
+ * Return: a pointer to the function that corresponds,
+ * to the operator given as parameter
+ */
+int (*get_op_func(char *s))(int, int)
+{
+
+	 op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+int i = 0;
+
+while (1 < 5)
+	{
+	if (strcmp(s, ops[i].op) == 0)
+	return (ops[i].f);
+	i++;
+	}
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,63 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * op_add - Functions that returns the sum of 2 int
+ * @a : first integer
+ * @b : second integer
+ *
+ * Return: Always (0) success
+ */
+int op_add(int a, int b)
+{
+
+	return (a + b);
+}
+/**
+ * op_sub - Function gives the difference of 2 integers
+ * @a : first integer
+ * @b : second integer
+ *
+ * Return: Always (0) success
+ */
+int op_sub(int a, int b)
+{
+
+	return (a - b);
+}
+/**
+ * op_mul -  Function gives the product of 2 integers
+ * @a : first integer
+ * @b : second integer
+ *
+ * Return: Always (0) success
+ */
+int op_mul(int a, int b)
+{
+
+	return (a * b);
+}
+/**
+ * op_div - function gives the division of 2 integers
+ * @a : first integer
+ * @b : second integer
+ *
+ * Return: Always (0) success
+ */
+int op_div(int a, int b)
+{
+
+	return (a / b);
+}
+/**
+ * op_mod - Function gives modulus
+ * @a : first integer
+ * @b : second integer
+ *
+ * Return: Always (0) success
+ */
+int op_mod(int a, int b)
+{
+
+	return (a % b);
+}
