a23b9ea0bdb5e58437e3e5f76c30b3b3df454d71
ritaOmondi <ritaOmondi>
2023-09-25 23:45:32 -0700

    rectified the loop

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,12 +8,14 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t countNodes = 0;
+	const listint_t *current = h;
 
-	while (h != NULL)
+	while (current != NULL)
 	{
-	printf("%d\n", h->n);
-	h = h->next;
+	printf("%d\n", current->n);
 	countNodes++;
+	current = current->next;
 	}
+	printf("\n");
 	return (countNodes);
 }
