4efd20ae2a5fdd39a1be4fc4e8b8c8a60f6dab39
ritaOmondi <ritaOmondi>
2023-10-02 08:19:05 -0700

    craeting a file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,28 +1,29 @@
 #include "main.h"
 #include <stdlib.h>
-#include<stdio.h>
+
 /**
- *read_textfile -  function reads textfile, prints to standard output,
- *to the POSIX.
- *@filename : pointer variable
- * @letters : number of characters to be read and printed
- * Return: writing- actual number of bytes read and printed
+ * read_textfile- Read text file print to STDOUT.
+ * @filename: text file being read
+ * @letters: number of letters to be read
+ * Return: w- actual number of bytes read and printed
  *        0 when function fails or filename is NULL.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-ssize_t File_Descriptor;
-char *buffer;
-ssize_t reading;
-ssize_t writing;
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
 
-File_Descriptor = open(filename, O_RDONLY);
-if (filename == NULL)
-return (0);
-buffer = malloc(sizeof(char) * letters);
-reading = read(File_Descriptor, buffer, letters);
-writing = write(STDOUT_FILENO, buffer, reading);
-free(buffer);
-close(File_Descriptor);
-return (writing);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
+
+	free(buf);
+	close(fd);
+	return (w);
 }
+
